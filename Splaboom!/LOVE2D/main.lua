a41886b27e1812cf1113eb5082138064576d8087
jrmaloc <jrmaloc@iskolarngbayan.pup.edu.ph>
2021-01-28 11:53:36 +0800

    timer

--- Splaboom!/LOVE2D/main.lua
+++ Splaboom!/LOVE2D/main.lua
@@ -12,7 +12,7 @@ local Wall = require 'wall'
 local Map = require 'map'
 local cron = require 'cron'
 
-local seconds = 60
+local seconds = 180
 local timer = cron.every(1, function() seconds = seconds - 1 end)
 love.graphics.setDefaultFilter("nearest", "nearest")
 
@@ -138,7 +138,7 @@ function love.draw() -----------------------------------------------------------
 	love.graphics.draw(arena, 43 - 15, 19 - 15, 0, 1, 1, 0, 0)
 	love.graphics.pop()
 
-	love.graphics.print("Timer: "..seconds, 5, 20)
+	love.graphics.print("Timer: "..seconds, 5, 25)
 end
 
 function love.keypressed(key)
